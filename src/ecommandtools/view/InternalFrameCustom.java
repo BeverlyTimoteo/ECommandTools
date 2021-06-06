1ded8e4d9d6d11896c6cbff03859c8c34c5623ae
Beverly Timóteo <beverlytimoteo@yahoo.com.br>
2021-06-06 11:36:50 -0300

    Ajuste nos imports

--- src/ecommandtools/view/InternalFrameCustom.java
+++ src/ecommandtools/view/InternalFrameCustom.java
@@ -1,7 +1,6 @@
 package ecommandtools.view;
 
 import ecommandtools.exception.ExceptionConfirm;
-import ecommandtools.exception.ExceptionCustom;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
